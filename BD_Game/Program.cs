d713433f74c259c0757963552703980151bba7f1
Drew Harshaw <drew.harshaw@queensu.ca>
2018-05-15 19:56:18 -0400

    The beginning

--- BD_Game/Program.cs
+++ BD_Game/Program.cs
@@ -4,19 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace BD_Game
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace SFML_Test
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
+
+        static void OnClose(object sender, EventArgs e)
+        {
+            // Close the window when OnClose event is received
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
+        }
+
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+            // Create the main window
+            RenderWindow app = new RenderWindow(new VideoMode(800, 600), "SFML Works!");
+            app.Closed += new EventHandler(OnClose);
+
+            Color windowColor = new Color(0, 192, 255);
+
+            // Start the game loop
+            while (app.IsOpen) {
+                // Process events
+                app.DispatchEvents();
+
+                // Clear screen
+                app.Clear(windowColor);
+
+                // Update the window
+                app.Display();
+            } //End game loop
+        } //End Main()
+    } //End Program
 }
