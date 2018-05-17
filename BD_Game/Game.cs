c9356051d90ccb133ec9e7526ea7338ecdf6f98c
Drew Harshaw <drew.harshaw@queensu.ca>
2018-05-17 00:01:18 -0400

    small changes

--- BD_Game/Game.cs
+++ BD_Game/Game.cs
@@ -7,14 +7,44 @@ using System.Threading.Tasks;
 namespace BD_Game{
     class Game {
 
-        
+        public uint timeInterval;
+        public uint timePadding;
+
+        //default constructor
+        Game(uint timeInterval, uint timePadding){
+            //create all peices
+            this.timeInterval = timeInterval;
+            this.timePadding = timePadding;
+
+            for (int i = 0; i < 7; i++){
+                Pawn B_pawn = new Pawn();
+            }
+        }
 
 
-//sdfsdfsdf
     }
 
-    class Common {
-        public static 
+    
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- BD_Game/Pieces.cs
+++ BD_Game/Pieces.cs
@@ -10,48 +10,60 @@ namespace BD_Game {
 
         public uint positionX;
         public uint positionY;
-        public string colour;
+        private string colour;
+
+        //Accessors
+     
+        public string getColour() {
+            return this.colour;
+        }
+
+        public void setColour(string col) {
+            this.colour = col;
+        }
 
     }
 
     public class Pawn : Piece{
 
         //default constructor
-        public Pawn (uint xCoord, uint yCoord, ref Piece[][] boardPosition) {
+        public Pawn (string colour, uint xCoord, uint yCoord, ref Piece[][] boardPosition) {
+
+            //set colour
+            this.setColour(colour);
 
             //set current position
             this.positionX = xCoord;
             this.positionY = yCoord;
+            
         }
 
 
-        public int LegalMoves() {
+        public int LegalMoves(ref Piece[][] boardPosition) {
 
             //check left attacking square
-            if (positionX > 0) {
+            if (positionX > 0 && boardPosition[positionX - 1][positionY + 1] != null) {
                 //if enemy
-                if(boardPosition[positionX-1][positionY+1] != null && !(getColour().Equals(this.colour))) {
+                if(!(boardPosition[positionX - 1][positionY + 1].getColour().Equals(this.getColour()))) { // had at just colour
                     //add to legal moves
                     
                 }
             }
             //check right attaching square
-            if (positionX < 7){
+            if (positionX < 7 && boardPosition[positionX + 1][positionY + 1] != null) {
                 //if enemy
-                if (boardPosition[positionX + 1][positionY + 1] != null && !(getColour().Equals(this.colour))) {
+                if (!(boardPosition[positionX + 1][positionY + 1].getColour().Equals(this.getColour()))) {
                     //add to legal moves
                 }
             }
 
-            //2 move rule
-            if (positionY == 1) {
-                //add to legal moves
-                
-            }
-
             //1 move rule
-            if(boardPosition[positionX][positionY + 1] != null) {
+            if(boardPosition[positionX][positionY + 1] == null) {
                 //add to legal moves
+
+            } else if (positionY == 1 && boardPosition[positionX][positionY + 2] == null) { // 2 move rull
+                //add to legal moves
+
             }
         }
     }
