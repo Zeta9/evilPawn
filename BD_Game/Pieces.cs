a745f1c38ba940b3d96c8d81c931124982c8bc0c
Drew Harshaw <drew.harshaw@queensu.ca>
2018-05-24 23:07:12 -0400

    added Bishop functionality

--- BD_Game/Pieces.cs
+++ BD_Game/Pieces.cs
@@ -70,7 +70,7 @@ namespace Pieces {
 
 
         //constructor
-        public Knight(string colour, uint xCoord, uint yCoord, ref Piece[][] boardPositio) {
+        public Knight(string colour, uint xCoord, uint yCoord, ref Piece[][] boardPosition) {
 
             //set colour
             this.colour = colour;
@@ -135,6 +135,120 @@ namespace Pieces {
 
     class Bishop : Piece {
 
+
+        public Bishop (string colour, uint xCoord, uint yCoord, ref Piece[][] boardPosition) {
+            //set colour
+            this.colour = colour;
+
+            //set current position
+            this.positionX = xCoord;
+            this.positionY = yCoord;
+
+        }
+
+        public void CalculateLegalMoves(ref string[][] BoardPosition) {
+
+
+            bool P1 = true, P2 = true, P3 = true, P4 = true;
+            int i = 1;
+
+            while(P1 == true && P2 == true && P3 == true && P4 == true) {
+
+                //check Left boundary
+                if (this.positionX - i < 0) {
+                    P1 = false;
+                    P3 = false;
+                }
+
+                //check Right boundary
+                if (this.positionX + i > 7) {
+                    P2 = false;
+                    P4 = false;
+                }
+            
+                //check Top boundary
+                if (this.positionY + i > 7) {
+                    P1 = false;
+                    P2 = false;
+                }
+
+                //check Bottom boundary
+                if (this.positionY - i < 0) {
+                    P3 = false;
+                    P4 = false;
+                }
+
+                // check path Top right
+                if (BoardPosition[this.positionX + i][this.positionY + i] != null) {
+                    //check if enemy ... add to list
+                    if (!BoardPosition[this.positionX + i][this.positionY + i].Equals(this.colour)) {
+                        //add legal move
+                        legalMoves.Add(new Position((uint)(this.positionX + i), (uint)(this.positionY + i)));
+                       
+                    }
+                    
+                    //close path
+                    P2 = true;
+                } else {
+                    //add legal move
+                    legalMoves.Add(new Position((uint)(this.positionX + i), (uint)(this.positionY + i)));
+                }
+
+
+                // check path Bottom right
+                if (BoardPosition[this.positionX + i][this.positionY - i] != null) {
+                    //check if enemy ... add to list
+                    if (!BoardPosition[this.positionX + i][this.positionY - i].Equals(this.colour)) {
+                        //add legal move
+                        legalMoves.Add(new Position((uint)(this.positionX + i), (uint)(this.positionY - i)));
+
+                    }
+
+                    //close path
+                    P4 = true;
+
+                } else {
+                    //add legal move
+                    legalMoves.Add(new Position((uint)(this.positionX + i), (uint)(this.positionY - i)));
+                }
+
+                // check path Top left
+                if (BoardPosition[this.positionX - i][this.positionY + i] != null) {
+                    //check if enemy ... add to list
+                    if (!BoardPosition[this.positionX - i][this.positionY + i].Equals(this.colour)) {
+                        //add legal move
+                        legalMoves.Add(new Position((uint)(this.positionX - i), (uint)(this.positionY + i)));
+
+                    }
+
+                    //close path
+                    P1 = true;
+
+                } else {
+                    //add legal move
+                    legalMoves.Add(new Position((uint)(this.positionX - i), (uint)(this.positionY + i)));
+                }
+
+                // check path Bottom left
+                if (BoardPosition[this.positionX - i][this.positionY - i] != null) {
+                    //check if enemy ... add to list
+                    if (!BoardPosition[this.positionX - i][this.positionY - i].Equals(this.colour)) {
+                        //add legal move
+                        legalMoves.Add(new Position((uint)(this.positionX - i), (uint)(this.positionY - i)));
+
+                    }
+
+                    //close path
+                    P3 = true;
+
+                } else {
+                    //add legal move
+                    legalMoves.Add(new Position((uint)(this.positionX - i), (uint)(this.positionY - i)));
+                }
+
+                i++; //increment counter
+            }
+        }
     }
 
 
